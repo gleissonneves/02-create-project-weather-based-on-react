855f65cf0f8aba5626368a435e9113ad3a76b1f2
gleissonneves <gleissonneves2017@gmail.com>
2022-03-21 23:30:42 -0300

    feat: base do projeto

---
+++ src/components/Icon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function defineIcon() {
+    
+}
+
+const Icon = function icon() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Icon;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 
 
-// https://www.behance.net/gallery/137181181/Fashion-Weather-Mobile-App-UIUX-design?tracking_source=search_projects%7Cweather
-// https://console.hgbrasil.com/documentation/weather/tools
-// https://console.hgbrasil.com/documentation/weather
-// https://api.hgbrasil.com/weather?woeid=455880
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
