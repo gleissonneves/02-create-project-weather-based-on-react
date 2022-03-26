8e1f5ab0fdaf53c6ad772eb6e5c8ddf2e404ef41
gleissonneves <gleissonneves2017@gmail.com>
2022-03-26 04:15:50 -0300

    fix(network): removendo camada http do projeto

--- src/network/api/endpoint.js
+++
@@ -1,9 +0,0 @@
-const { process: { env } } = require('dotenv').load()
-const { PUBLIC_API_WEATHER } = env
-
-const endpoints = {
-    findCity: `${PUBLIC_API_WEATHER}/stats/find_woeid`,
-    climate: `${PUBLIC_API_WEATHER}/weather`,
-}
-
-export default endpoints;
