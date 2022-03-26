35b8ca69edf6d23f0f3bf55eb148a183ca06a2fa
Leonardo Lima Coelho <ryszol@protonmail.com>
2022-03-26 18:44:06 -0400

    feat: criando função para pegar os dados da api. Exemplo de uso no component Icon. Criando arquivo .env.example Novas regras no eslint. Removendo o .env do git.

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getWeather } from '../services/api';
 
 const Icon = function icon() {
-  return <h1>olá mundo</h1>;
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getWeather(setData);
+  }, []);
+
+  return <h1>{data ? data.results.description : 'Carregando...'}</h1>;
 };
 
 export default Icon;
--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,14 +1,36 @@
-function getWeather() {
-  fetch('https://api.hgbrasil.com/weather?key=2e633e63&woeid=455880', {
-    headers: {
-      'Access-Control-Allow-Origin': '200.140.90.228',
+/**
+ * Esta função busca os dados do clima da sua cidade.
+ *
+ * @param {*} setValue
+ * [obrigatório] função do useState.
+ * @param {*} errorFunction
+ * [opcional] Função que executará caso o usuário negue
+ * a localização (pode receber o parâmetro de erro).
+ */
+
+const { REACT_APP_API_WEATHER: api, REACT_APP_API_KEY: key } = process.env;
+
+function getWeather(setValue, error) {
+  navigator.geolocation.getCurrentPosition(
+    async (position) => {
+      const { latitude, longitude } = position.coords;
+
+      const response = await fetch(
+        `${api}?format=json-cors&key=${key}&lat=${latitude}&lon=${longitude}`,
+      );
+
+      const result = await response.json();
+
+      setValue(result);
     },
-  })
-    .then((res) => res.json())
-    .then(
-      (result) => result,
-      (error) => error,
-    );
+    (err) => {
+      if (error) {
+        error(err);
+      } else {
+        console.warn(`Error ${err.code}: ${err.message}`);
+      }
+    },
+  );
 }
 
-export default getWeather;
+export { getWeather };
